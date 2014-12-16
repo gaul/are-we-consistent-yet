f05ffebf345b2fbc8374b59eaf0b190b462fedb6
Andrew Gaul <andrew@gaul.org>
2014-12-15 18:14:32 -0800

    Initial commit of Are We Consistent Yet

---
+++ README.md
@@ -0,0 +1,50 @@
+# Are We Consistent Yet?
+
+Observed and documented eventual consistency of object stores, e.g., Amazon S3,
+OpenStack Swift.
+
+## What is eventual consistency?
+
+Traditional systems provide
+[strong consistency](https://en.wikipedia.org/wiki/Strong_consistency), where
+clients can immediately view updates.
+Some distributed systems relax their consistency model to allow greater
+availability or better performance.
+[Eventual consistency](https://en.wikipedia.org/wiki/Eventual_consistency)
+manifests itself to clients as stale views of data.
+
+## Observed consistency
+
+Observed instances of eventual consistency with a number of operations during
+tests around 17 December 2014:
+
+| Provider | read after create | read after delete | read after overwrite | list after create | list after delete | number of operations |
+| --- | :---: | :---: | :---: | :---: | :---: | :---: |
+| Amazon S3 (us-standard)     | * |  * | * | 16 |  * | 100,000 |
+| Amazon S3 (us-west)         | - |  1 | 1 |  * |  * | 100,000 |
+| Ceph (DreamObjects)         | - |  - | - |  - |  - |   1,000 |
+| Google Cloud Storage        | - |  - | - |  2 |  2 |   1,000 |
+| Microsoft Azure Storage     | - |  - | - |  - |  - |   1,000 |
+| OpenStack Swift (Rackspace) | - | 17 | 3 | 30 | 20 |   1,000 |
+
+Legend:
+
+* - zero observed instances
+* * zero observed instances but expect non-zero
+
+## Documented consistency
+
+* [Amazon S3](http://aws.amazon.com/s3/faqs/#What_data_consistency_model_does_Amazon_S3_employ) - buckets in the US Standard region provide eventual consistency.  Buckets in all other regions provide read-after-write consistency for PUTs of new objects and eventual consistency for overwrite PUTs and DELETEs.
+* [Ceph](http://ceph.com/papers/weil-rados-pdsw07.pdf) - provid[es] ... well-defined safety semantics and strong consistency guarantees.
+* [Google Cloud Storage](https://cloud.google.com/storage/docs/concepts-techniques#consistency) - provides strong global consistency for all read-after-write, read-after-update, and read-after-delete operations, including both data and metadata.  List operations are eventually consistent.
+* [Microsoft Azure Storage](http://azure.microsoft.com/blog/2014/09/08/managing-concurrency-in-microsoft-azure-storage-2/) - was designed to embrace a strong consistency model which guarantees that when the Storage service commits a data insert or update operation all further accesses to that data will see the latest update.
+* [OpenStack Swift](http://docs.openstack.org/developer/swift/overview_architecture.html#updaters) - For example, suppose a container server is under load and a new object is put in to the system. The object will be immediately available for reads as soon as the proxy server responds to the client with success. However, the container server did not update the object listing, and so the update would be queued for a later update. Container listings, therefore, may not immediately contain the object.  [Additional reference](http://lists.openstack.org/pipermail/openstack-dev/2014-June/038881.html).
+
+## References
+
+* [Apache jclouds](https://jclouds.apache.org/) - provides object storage support
+* [A Middleware Guaranteeing Client-Centric Consistency on Top of Eventually
+Consistent Datastores](http://www.aifb.kit.edu/images/4/44/Ic2e2013consistency.pdf)
+* [Benchmarking Eventual Consistency: Lessons Learned from Long-Term Experimental Studies](http://www.aifb.kit.edu/images/8/8d/Ic2e2014.pdf)
+* [Eventual Consistency: How soon is eventual?](http://www.researchgate.net/publication/259541556_Eventual_Consistency_How_soon_is_eventual_An_Evaluation_of_Amazon_S3%27s_Consistency_Behavior/links/0deec52c6e04b49921000000)
+* [Probabilistically Bounded Staleness](http://pbs.cs.berkeley.edu/) - includes discussion of Riak and Cassandra
---
+++ src/main/java/org/gaul/areweconsistentyet/AreWeConsistentYet.java
@@ -0,0 +1,269 @@
+/*
+ * Copyright 2014 Andrew Gaul <andrew@gaul.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gaul.areweconsistentyet;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Properties;
+import java.util.Random;
+import java.util.Set;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableList;
+import com.google.common.io.ByteSource;
+import com.google.common.io.ByteStreams;
+import com.google.inject.Module;
+
+import org.jclouds.Constants;
+import org.jclouds.ContextBuilder;
+import org.jclouds.blobstore.BlobStore;
+import org.jclouds.blobstore.BlobStoreContext;
+import org.jclouds.blobstore.domain.Blob;
+import org.jclouds.blobstore.domain.PageSet;
+import org.jclouds.blobstore.domain.StorageMetadata;
+import org.jclouds.blobstore.options.ListContainerOptions;
+import org.jclouds.domain.Location;
+import org.jclouds.io.Payload;
+import org.jclouds.logging.slf4j.config.SLF4JLoggingModule;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+
+public final class AreWeConsistentYet {
+    private static final ByteSource payload1 = ByteSource.wrap(new byte[] {1});
+    private static final ByteSource payload2 = ByteSource.wrap(new byte[] {2});
+    private final BlobStore blobStore;
+    private final String containerName;
+    private final int iterations;
+    private final Random random = new Random();
+
+    public AreWeConsistentYet(BlobStore blobStore, String containerName,
+            int iterations) {
+        this.blobStore = Preconditions.checkNotNull(blobStore);
+        this.containerName = Preconditions.checkNotNull(containerName);
+        Preconditions.checkArgument(iterations > 0,
+                "iterations must be greater than zero, was: " + iterations);
+        this.iterations = iterations;
+    }
+
+    public int readAfterCreate() throws IOException {
+        int count = 0;
+        for (int i = 0; i < iterations; ++i) {
+            String blobName = makeBlobName();
+            blobStore.putBlob(containerName, makeBlob(blobName, payload1));
+            Blob getBlob = blobStore.getBlob(containerName, blobName);
+            if (getBlob == null) {
+                ++count;
+            } else {
+                try (Payload payload = getBlob.getPayload();
+                     InputStream is = payload.openStream()) {
+                    ByteStreams.copy(is, ByteStreams.nullOutputStream());
+                }
+            }
+            blobStore.removeBlob(containerName, blobName);
+        }
+        return count;
+    }
+
+    public int readAfterDelete() throws IOException {
+        int count = 0;
+        for (int i = 0; i < iterations; ++i) {
+            String blobName = makeBlobName();
+            blobStore.putBlob(containerName, makeBlob(blobName, payload1));
+            blobStore.removeBlob(containerName, blobName);
+            Blob getBlob = blobStore.getBlob(containerName, blobName);
+            if (getBlob != null) {
+                ++count;
+                try (Payload payload = getBlob.getPayload();
+                     InputStream is = payload.openStream()) {
+                    ByteStreams.copy(is, ByteStreams.nullOutputStream());
+                }
+            }
+        }
+        return count;
+    }
+
+    public int readAfterOverwrite() throws IOException {
+        int count = 0;
+        for (int i = 0; i < iterations; ++i) {
+            String blobName = makeBlobName();
+            blobStore.putBlob(containerName, makeBlob(blobName, payload1));
+            blobStore.putBlob(containerName, makeBlob(blobName, payload2));
+            Blob getBlob = blobStore.getBlob(containerName, blobName);
+            try (Payload payload = getBlob.getPayload();
+                 InputStream is = payload.openStream()) {
+                if (Arrays.equals(payload1.read(), ByteStreams.toByteArray(
+                        is))) {
+                    ++count;
+                }
+            }
+            blobStore.removeBlob(containerName, blobName);
+        }
+        return count;
+    }
+
+    public int listAfterCreate() throws IOException {
+        int count = 0;
+        for (int i = 0; i < iterations; ++i) {
+            String blobName = makeBlobName();
+            blobStore.putBlob(containerName, makeBlob(blobName, payload1));
+            if (!listAllBlobs().contains(blobName)) {
+                ++count;
+            }
+            blobStore.removeBlob(containerName, blobName);
+        }
+        return count;
+    }
+
+    public int listAfterDelete() throws IOException {
+        int count = 0;
+        for (int i = 0; i < iterations; ++i) {
+            String blobName = makeBlobName();
+            blobStore.putBlob(containerName, makeBlob(blobName, payload1));
+            blobStore.removeBlob(containerName, blobName);
+            if (listAllBlobs().contains(blobName)) {
+                ++count;
+            }
+        }
+        return count;
+    }
+
+    private String makeBlobName() {
+        return "blob-name-" + random.nextInt();
+    }
+
+    private Blob makeBlob(String blobName, ByteSource payload)
+            throws IOException {
+        return blobStore.blobBuilder(blobName)
+                .payload(payload)
+                .contentLength(payload.size())
+                .build();
+    }
+
+    private Set<String> listAllBlobs() {
+        Set<String> blobNames = new HashSet<String>();
+        ListContainerOptions options = new ListContainerOptions();
+        while (true) {
+            PageSet<? extends StorageMetadata> set = blobStore.list(
+                    containerName, options);
+            for (StorageMetadata sm : set) {
+                blobNames.add(sm.getName());
+            }
+            String marker = set.getNextMarker();
+            if (marker == null) {
+                break;
+            }
+            options = options.afterMarker(marker);
+        }
+        return blobNames;
+    }
+
+    static final class AreWeConsistentYetOptions {
+        @Option(name = "--container-name",
+                usage = "container name for tests, will be created and removed",
+                required = true)
+        private String containerName = "container-name";
+
+        @Option(name = "--iterations", usage = "number of iterations")
+        private int iterations = 1;
+
+        @Option(name = "--location", usage = "container location")
+        private String location;
+
+        @Option(name = "--properties", usage = "configuration file",
+                required = true)
+        private File propertiesFile;
+    }
+
+    public static void main(String[] args) throws Exception {
+        AreWeConsistentYetOptions options = new AreWeConsistentYetOptions();
+        CmdLineParser parser = new CmdLineParser(options);
+        try {
+            parser.parseArgument(args);
+        } catch (CmdLineException cle) {
+            PrintStream err = System.err;
+            err.println("are-we-consistent-yet version " +
+                    AreWeConsistentYet.class.getPackage()
+                            .getImplementationVersion());
+            err.println("Usage: are-we-consistent-yet" +
+                    " --container-name NAME --properties FILE [options...]");
+            parser.printUsage(err);
+            System.exit(1);
+        }
+
+        Properties properties = new Properties();
+        try (InputStream is = new FileInputStream(options.propertiesFile)) {
+            properties.load(is);
+        }
+
+        String provider = properties.getProperty(Constants.PROPERTY_PROVIDER);
+        String identity = properties.getProperty(Constants.PROPERTY_IDENTITY);
+        String credential = properties.getProperty(
+                Constants.PROPERTY_CREDENTIAL);
+        String endpoint = properties.getProperty(Constants.PROPERTY_ENDPOINT);
+        if (provider == null || identity == null || credential == null) {
+            System.err.println("Properties file must contain:\n" +
+                    Constants.PROPERTY_PROVIDER + "\n" +
+                    Constants.PROPERTY_IDENTITY + "\n" +
+                    Constants.PROPERTY_CREDENTIAL);
+            System.exit(1);
+        }
+
+        ContextBuilder builder = ContextBuilder
+                .newBuilder(provider)
+                .credentials(identity, credential)
+                .modules(ImmutableList.<Module>of(new SLF4JLoggingModule()))
+                .overrides(properties);
+        if (endpoint != null) {
+            builder = builder.endpoint(endpoint);
+        }
+        try (BlobStoreContext context = builder.build(BlobStoreContext.class)) {
+            BlobStore blobStore = context.getBlobStore();
+            Location location = null;
+            if (options.location != null) {
+                for (Location loc : blobStore.listAssignableLocations()) {
+                    if (loc.getId().equalsIgnoreCase(options.location)) {
+                        location = loc;
+                        break;
+                    }
+                }
+                if (location == null) {
+                    throw new Exception("Could not find location: " +
+                            options.location);
+                }
+            }
+            blobStore.createContainerInLocation(location,
+                    options.containerName);
+            AreWeConsistentYet test = new AreWeConsistentYet(
+                    blobStore, options.containerName, options.iterations);
+            PrintStream out = System.out;
+            out.println("eventual consistency count with " +
+                    options.iterations + " iterations: ");
+            out.println("read after create: " + test.readAfterCreate());
+            out.println("read after delete: " + test.readAfterDelete());
+            out.println("read after overwrite: " + test.readAfterOverwrite());
+            out.println("list after create: " + test.listAfterCreate());
+            out.println("list after delete: " + test.listAfterDelete());
+            blobStore.deleteContainer(options.containerName);
+        }
+    }
+}
